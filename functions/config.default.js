1b2e7b39cf2555f07bd6e5f32ca87a73f4eb31ab
Mahasak Pijittum <mahasak@gmail.com>
2018-11-25 00:32:13 +0800

    add webhook verify function

---
+++ functions/config.default.js
@@ -0,0 +1,10 @@
+const config = {
+    facebook: {
+        app_id: '<APP_ID>',
+        app_secret: '',
+        page_access_token: '<PAGE_ACCESS_TOKEN>',
+        validation_token: '<VERIFY_TOKEN>',
+    }
+};
+
+module.exports = config;
--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,25 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const config = require('./config')
+const admin = require('firebase-admin')
+const functions = require('firebase-functions')
+const rp = require('request-promise')
+
+admin.initializeApp(functions.config().firebase)
+
+exports.webhook = functions.https.onRequest((req, res) => {
+    verifySubscription(req, res)
+
+    res.sendStatus(200)
+})
+
+const verifySubscription = (req, res) => {
+    if (req.query['hub.mode'] === 'subscribe' &&
+        req.query['hub.verify_token'] === config.validation_token) {
+        console.log("Validating webhook")
+        res.status(200).send(req.query['hub.challenge'])
+    } else {
+        console.error("Failed validation. Make sure the validation tokens match.")
+        res.sendStatus(403)
+    }
+}
