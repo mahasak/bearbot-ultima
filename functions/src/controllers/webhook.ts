076aeb55488650669fd46ca20b144a8afa33f5f2
Mahasak Pijittum <mahasak@gmail.com>
2019-06-26 23:45:02 +0800

    change webhook to use pipeline

--- functions/src/controllers/webhook.ts
+++ functions/src/controllers/webhook.ts
@@ -1,16 +1,8 @@
 import { Request, Response } from 'firebase-functions';
 import { VerifySubscriptionRule, DefaultRule } from "../rules";
 import { IWebhookRule } from '../rules/IWebhookRule';
+import {WebhookPipeline} from '../components/WebhookPipeline';
 
-const pipeline: IWebhookRule[] = [VerifySubscriptionRule.getInstance(), DefaultRule.getInstance()];
-
-export let webhook = (request: Request, response: Response) => { 
-    let executed: boolean = false;
-
-    pipeline.forEach(rule => {
-        if(!executed && rule.pass(request, response)){
-            rule.execute(request, response);
-            executed = true;
-        }
-    });
-}
+//const pipeline: IWebhookRule[] = [VerifySubscriptionRule.getInstance(), DefaultRule.getInstance()];
+const pipeline = new WebhookPipeline([VerifySubscriptionRule.getInstance(), DefaultRule.getInstance()]);
+export let webhook = (request: Request, response: Response) => pipeline.run(request, response)
