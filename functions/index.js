f183c222fee2f782e2bc348a8743b8f1309d0f90
Mahasak Pijittum <mahasak@gmail.com>
2019-07-14 00:38:26 +0800

    update webhook

--- functions/index.js
+++ functions/index.js
@@ -11,17 +11,127 @@ exports.version = functions.https.onRequest((request, response) => {
 })
 
 exports.webhook = functions.https.onRequest((req, res) => {
-    switch (req.method) {
-        case 'GET':
-            verifySubscription(req, res)
-            break
-        case 'POST':
-            processMessage(req, res)
-            break
+    const body = req.body
+    if (body.object === 'page') {
+        body.entry.forEach(function (entry) {
+
+            // Get the webhook event. entry.messaging is an array, but 
+            // will only ever contain one event, so we get index 0
+            let webhook_event = entry.messaging[0];
+            console.log(webhook_event);
+
+            // Get the sender PSID
+            let sender_psid = webhook_event.sender.id;
+            console.log('Sender PSID: ' + sender_psid);
+
+            // Check if the event is a message or postback and
+            // pass the event to the appropriate handler function
+            if (webhook_event.message) {
+                handleMessage(sender_psid, webhook_event.message);
+            } else if (webhook_event.postback) {
+                handlePostback(sender_psid, webhook_event.postback);
+            }
+
+        });
+
+        res.status(200).send('EVENT_RECEIVED');
+
+    } else {
+        res.sendStatus(404);
+    }
+})
+
+const handleMessage = (sender_psid, received_message) => {
+    let response;
+
+    // Checks if the message contains text
+    if (received_message.text) {
+        // Create the payload for a basic text message, which
+        // will be added to the body of our request to the Send API
+        response = {
+            "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
+        }
+    } else if (received_message.attachments) {
+        // Get the URL of the message attachment
+        let attachment_url = received_message.attachments[0].payload.url;
+        response = {
+            "attachment": {
+                "type": "template",
+                "payload": {
+                    "template_type": "generic",
+                    "elements": [{
+                        "title": "Is this the right picture?",
+                        "subtitle": "Tap a button to answer.",
+                        "image_url": attachment_url,
+                        "buttons": [
+                            {
+                                "type": "postback",
+                                "title": "Yes!",
+                                "payload": "yes",
+                            },
+                            {
+                                "type": "postback",
+                                "title": "No!",
+                                "payload": "no",
+                            }
+                        ],
+                    }]
+                }
+            }
+        }
     }
 
-    res.sendStatus(200)
-})
+    // Send the response message
+    callSendAPI(sender_psid, response);
+}
+
+const handlePostback = (sender_psid, received_postback) => {
+    let response;
+
+    // Get the payload for the postback
+    let payload = received_postback.payload;
+
+    // Set the response based on the postback payload
+    if (payload === 'yes') {
+        response = { "text": "Thanks!" }
+    } else if (payload === 'no') {
+        response = { "text": "Oops, try sending another image." }
+    }
+    // Send the message to acknowledge the postback
+    callSendAPI(sender_psid, response);
+}
+
+const callSendAPI = (psid, response) => {
+    let request_body = {
+        recipient: {
+            id: psid
+        },
+        message: response
+    }
+
+    request({
+        uri: 'https://graph.facebook.com/v3.3/me/messages',
+        qs: { access_token: process.env.pagetoken },
+        method: "POST",
+        json: request_body
+    }, (error, response, body) => {
+        if (!error && response.statusCode == 200) {
+            var recipientId = body.recipient_id;
+            var messageId = body.message_id
+
+            if (messageId) {
+                console.log("Successfully sent message with id %s to recipient %s",
+                    messageId, recipientId)
+            } else {
+                console.log("Successfully called Send API for recipient %s",
+                    recipientId)
+            }
+        } else {
+            console.error("Failed calling Send API", response.statusCode, response.statusMessage, body.error)
+        }
+    })
+}
+
 
 const verifySubscription = (req, res) => {
     if (req.query['hub.mode'] === 'subscribe' &&
@@ -90,7 +200,7 @@ const receivedMessage = (event) => {
     markSeen(pageScopeID)
 
     // disable echo message
-    
+
 }
 
 const receivedDeliveryConfirmation = (event) => {
@@ -135,34 +245,7 @@ const sendTextMessage = (recipientId, messageText) => {
     callSendAPI(messageData)
 }
 
-const callSendAPI = (messageData) => {
-    request({
-        uri: 'https://graph.facebook.com/v3.3/me/messages',
-        qs: { access_token: process.env.pagetoken },
-        method: 'POST',
-        json: messageData
 
-    }, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            var recipientId = body.recipient_id;
-            var messageId = body.message_id
-
-            if (messageId) {
-                console.log("Successfully sent message with id %s to recipient %s",
-                    messageId, recipientId)
-            } else {
-                console.log("Successfully called Send API for recipient %s",
-                    recipientId)
-            }
-        } else {
-            console.error("Failed calling Send API", response.statusCode, response.statusMessage, body.error)
-        }
-    });
-}
-
-const callUserAPI = (userId) => {
-    
-}
 
 const markSeen = (psid) => {
     console.log("Sending a read receipt to mark message as seen");
